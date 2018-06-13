5f094c7022e19e48e698ce2b6926cb8c8b37869e
Gregory N. Schmit <schmitgreg@gmail.com>
2018-06-13 10:32:27 -0500

    improve secure password generation

--- wav_admins.rb
+++ wav_admins.rb
@@ -10,7 +10,6 @@ admin_and_public_keys = {
   'gns': 'ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAizOg1IZjezR22uwK7Qp4Qa1uGxS+y+3ae8hIyzq8u3TpvqbLcbZ/dq+22b+CEOYAWRUhfgM/5ltRdsVCzMEgkvL8w+jLdv7rxRtvHp1XhSjsOKW5pQoiiTetOlOUfNQ+eIPh78kcgHIkg3Z9ISz86mL840e4FWs/wyD2GSvskjEVUT5l6K7gDjJuVMFsEc49A9B6rePpjRDSCwQ3n7/TSwGkA5tsTnt27HvKbz3UQ6mNsg/QZmLVWiWDaMk3vKQEWPn+pbSOkBYXhT6mye05235nyu4f1rvzbd51Ko0ZBhiDoNArhpuF9KRCllip0QpETR1DJd15IB38K0CYE1A4NQ==',
   'sra': 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCnXlUQLDvYlCUTNhoSQAYpnKt82nlP7phW5lzdBIA0MY8PWUOIve5UGrubIo/RJOrCKZtMSD+GnzkV0Kq7+gr3MwfT9xPx3udN2yjCcliTG46DGQYAe6BSOPt2Qh/1ZaoAjHOMSsxUuEH3RtbfHpE5emWLx+9RNBMkKxYhcXks7hah8l0fAsk4zJXkBsxlI+sYlAiVTw5b/ejCsk/sz/Oyh5yUyNiH5FBGfIe15rtFZC+hrsRufjDsXA+bWd+P5KPiq2PgQnyA2j+Lkp28amGQv+oMZTMkcbL1r8JcYXyf8QaIM0AC+LWWg5S0bT1l1lWk7EpP3FVjlOlKApGat+KR',
 }
-chrs = [*48..57] + [*65..90] + [*97..122];0 # suppress return when pasting into irb
 
 # WAV admin role
 ar = AdminRole.find_or_create_by(name: 'WAV')
@@ -30,7 +29,7 @@ ar.save!
 admin_and_public_keys.each do |admin, public_key|
   a = Admin.find_or_create_by(login: admin)
   if not a.crypted_password
-    pw = (0..20).map {chrs[rand(chrs.length)].chr}.join
+    pw = rand(36**16).to_s(36)
     a.password = pw
     a.password_confirmation = pw
   end
