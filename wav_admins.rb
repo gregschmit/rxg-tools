0f40acee90c6da58d36d2cb952f62dd7b19399fe
Gregory N. Schmit <schmitgreg@gmail.com>
2018-11-08 08:56:49 -0600

    fix ssh key not working bug

--- wav_admins.rb
+++ wav_admins.rb
@@ -30,7 +30,8 @@ ar.save!
 
 # WAV admins
 admin_and_public_keys.each do |admin, public_key|
-  a = Admin.find_or_create_by(login: admin)
+  a = Admin.find_by(login: admin) || Admin.new
+  a.login = admin
   if not a.crypted_password
     pw = rand(36**16).to_s(36).rjust(16, '0')
     a.password = pw
